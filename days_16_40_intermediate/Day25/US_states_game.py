ff156625af7efbcdd2931ca26a6c4fdb8c3aba2b
Robprogram2002 <robertomr@ciencias.unam.mx>
2022-10-10 14:32:04 -0500

    were created a file to practice working with csv files using python, pandas package and some samples datasets. Also were created a guessing U.S states game using turtle package and pandas to handle data. This was the project of the day 25

---
+++ days_16_40_intermediate/Day25/US_states_game.py
@@ -0,0 +1,47 @@
+import pandas
+from turtle import Screen, shape, mainloop, Turtle
+
+screen = Screen()
+screen.title("U.S States Game")
+image = "blank_states_img.gif"
+screen.addshape(image)
+shape(image)
+
+# with these functions we can create the data in the 50_states.csv file
+
+# def get_mouse_click_coord(x, y):
+#     print(x, y)
+
+# onscreenclick(get_mouse_click_coord)
+
+data = pandas.read_csv("50_states.csv")
+states = data.state.to_list()
+guessed_states = []
+
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States correct",
+                                    prompt="What's another state's name? ").title()
+
+    if answer_state == "Exit":
+        # saves state to learn in a csv file for further study
+        missing_states = []
+        for state in states:
+            if state not in guessed_states:
+                missing_states.append(state)
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("states_to_learn.csv")
+        break
+    if answer_state in states:
+        t = Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer_state]
+        t.goto(int(state_data.x), int(state_data.y))
+        t.write(answer_state)
+        guessed_states.append(answer_state)
+
+# improvements :
+# include an automatic save of the already guessed states. So you can run the game again later and complete it
+# Added the missing states to the map in red
+
+
